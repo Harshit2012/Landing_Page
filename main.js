5e1ace8ac845f38f2ab02c7d3e649faa51da6a85
Harshit2012 <105143145+Harshit2012@users.noreply.github.com>
2022-08-20 19:45:04 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+function Entry(){
+    window.location = "https://www.jotform.com/form-templates/grace-transformation-challenge-application"
+    console.log("Opening Gym")
+    alert("Do you want to Entry?")
+}
+
+function Contact(){
+    window.location = "https://mail.google.com/mail/u/0/#inbox"
+}
